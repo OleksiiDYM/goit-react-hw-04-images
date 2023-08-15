4aa9c80854d87e456a6450801c6e7d467da08748
Oleksii <al.dimitrash@gmail.com>
2023-08-15 14:40:02 +0300

    update

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,45 @@
+import { Component } from 'react';
+import { createPortal } from 'react-dom';
+import './Modal.css';
+import PropTypes from 'prop-types';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = event => {
+    if (event.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleBackDropClick = event => {
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return createPortal(
+      <div className="Overlay" onClick={this.handleBackDropClick}>
+        <div className="Modal">
+          <img src={this.props.largeImageURL} alt={this.props.tags} />
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
+}
+
+Modal.propTypes = {
+  tags: PropTypes.string.isRequired,
+  onClose: PropTypes.func.isRequired,
+  largeImageUrl: PropTypes.string,
+};
